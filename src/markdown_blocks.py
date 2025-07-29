02bbd92e012c28614ec93622c3b3d5183e778f69
notacoham <alexcottam12@gmail.com>
2025-07-29 11:23:34 -0600

    add blocktypes and conversions along with tests

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,16 +1,47 @@
+from enum import Enum
+import re
+
+class BlockType(Enum):
+    PARAGRAPH = 'paragraph'
+    HEADING = 'heading'
+    CODE = 'code'
+    QUOTE = 'quote'
+    UNORDERED_LIST = 'unordered_list'
+    ORDERED_LIST = 'ordered_list'
+
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return BlockType.HEADING
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+        return BlockType.CODE
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return BlockType.PARAGRAPH
+        return BlockType.UNORDERED_LIST
+    if block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return BlockType.PARAGRAPH
+            i += 1
+        return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
+
+
 def markdown_to_blocks(markdown):
-    lines = markdown.split('\n\n')
+    lines = markdown.split("\n\n")
     processed_lines = []
     for line in lines:
-        processed_lines.append(line.strip())
+        stripped = line.strip()
+        if stripped != "":
+            processed_lines.append(stripped)
     return processed_lines
-
-markdown_to_blocks(
-'''# This is a heading
-
-This is a paragraph of text. It has some **bold** and _italic_ words inside of it.
-
-- This is the first list item in a list block
-- This is a list item
-- This is another list item'''
-)
